9a00fe0cf997deecbb16b6371d640f34217b462e
Jozef <jozef.valko.285@gmail.com>
2017-11-01 22:13:35 +0100

    loading screen done

--- app/src/main/java/com/example/jozef/vcelicky/Loading_screen.java
+++ app/src/main/java/com/example/jozef/vcelicky/Loading_screen.java
@@ -1,7 +1,12 @@
 package com.example.jozef.vcelicky;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.github.ybq.android.spinkit.style.DoubleBounce;
 
 public class Loading_screen extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class Loading_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
+
+        CountDownTimer timer = new CountDownTimer(2000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        timer.start();
     }
 }

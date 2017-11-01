e64d560a62c2e23829580d5768a0329ee780a441
Jozef <jozef.valko.285@gmail.com>
2017-11-01 14:26:25 +0100

    loading screen and login in progress

--- app/src/main/java/com/example/jozef/vcelicky/Loading_screen.java
+++ app/src/main/java/com/example/jozef/vcelicky/Loading_screen.java
@@ -3,11 +3,11 @@ package com.example.jozef.vcelicky;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Log_screen extends AppCompatActivity {
+public class Loading_screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_screen);
+        setContentView(R.layout.activity_loading_screen);
     }
 }

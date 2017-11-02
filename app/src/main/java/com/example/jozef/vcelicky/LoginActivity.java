58fb312509c1b6c9bb2aca218df1970888371854
Jozef <jozef.valko.285@gmail.com>
2017-11-02 22:50:12 +0100

    error message added

--- app/src/main/java/com/example/jozef/vcelicky/LoginActivity.java
+++ app/src/main/java/com/example/jozef/vcelicky/LoginActivity.java
@@ -1,20 +1,45 @@
 package com.example.jozef.vcelicky;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import java.lang.invoke.ConstantCallSite;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText mail, pass;
+    ConstraintLayout main, error;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Log.v("LoginAct", "Startin' activity");
+        mail = findViewById(R.id.editMail);
+        pass = findViewById(R.id.editPass);
+        main = findViewById(R.id.mainLayout);
+        error = findViewById(R.id.errorLayout);
     }
 
     public void login (View view){
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
+        if(mail.getText().toString().equals("")){
+            main.setAlpha((float)0.5);
+            error.setVisibility(View.VISIBLE);
+        }
+        else {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public void close(View view){
+        error.setVisibility(View.INVISIBLE);
+        main.setAlpha(1);
     }
 }

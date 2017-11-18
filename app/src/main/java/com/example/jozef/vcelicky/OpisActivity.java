ead0161a80d29f984d79793d84a538e3750e6c2e
Jozef <jozef.valko.285@gmail.com>
2017-11-18 15:07:08 +0100

    created OpisActivity

---
+++ app/src/main/java/com/example/jozef/vcelicky/OpisActivity.java
@@ -0,0 +1,13 @@
+package com.example.jozef.vcelicky;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opis);
+    }
+}

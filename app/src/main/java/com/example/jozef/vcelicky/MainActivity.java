b614acfc4307423c396963b6c4c58174e0aeece5
Jakub Pullman <raptorsvk@gmail.com>
2017-11-19 17:55:08 +0100

    Hotfix: new menu items id changed

--- app/src/main/java/com/example/jozef/vcelicky/MainActivity.java
+++ app/src/main/java/com/example/jozef/vcelicky/MainActivity.java
@@ -110,19 +110,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_about_project) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_profile) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_notifications) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_logout) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+        } 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
